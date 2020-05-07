0be52d949f4a5984a5cf50c670caece1713210bf
konark kapil <konarkkapil@rocketmail.com>
2020-05-07 23:45:59 +0530

    Added add items to cart item number gets increased on adding more

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component'
+import {connect} from 'react-redux'
+import {addItem} from '../../redux/cart/cart.actions'
 
-const CollectionItem = ({id,name,price,imageUrl}) => {
+const CollectionItem = ({item,addItem}) => {
+    const {name,price,imageUrl} = item
     return(
         <div className="collection-item">
             <div className="image" style={{backgroundImage:`url(${imageUrl})`}} />
@@ -11,8 +15,13 @@ const CollectionItem = ({id,name,price,imageUrl}) => {
                 <span>{name}</span>
                 <span>{price}</span>
             </div>
+            <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
         </div>
     )
 }
 
-export default CollectionItem
+const mapDispatchToProps = dispatch =>({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null,mapDispatchToProps)(CollectionItem)
