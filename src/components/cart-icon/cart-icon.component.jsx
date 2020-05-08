37c0ba97761dde7f83f3f18ddd9ec5244fd71f15
konark kapil <konarkkapil@rocketmail.com>
2020-05-08 08:35:50 +0530

    added selector caching in cart component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,12 +3,13 @@ import {ReactComponent as ShoppingBag } from '../../assets/shopping-bag.svg'
 import "./cart-icon.styles.scss"
 import { connect } from 'react-redux'
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import {selectCartItemsCount} from '../../redux/cart/cart.selector'
 
-const cartIcon = ({toggleCartHidden}) => {
+const cartIcon = ({toggleCartHidden,itemCount}) => {
     return(
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingBag className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     )
 }
@@ -17,4 +18,8 @@ const mapDispatchToProps = dispatch =>({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null,mapDispatchToProps)(cartIcon)
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(cartIcon)
